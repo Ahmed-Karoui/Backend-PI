97f465aa9fdeb2161b238facdb7823b75a3b7394
skander5 <49730176+skander5@users.noreply.github.com>
2021-07-24 13:01:14 +0100

    SEND WITH PDF

--- src/main/java/tn/esprit/services/EmailServiceImpl.java
+++ src/main/java/tn/esprit/services/EmailServiceImpl.java
@@ -1,8 +1,14 @@
 package tn.esprit.services;
 
+import java.io.File;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,5 +25,23 @@ public class EmailServiceImpl {
 		message.setSubject(subject);
 		message.setText(text);
 		emailSender.send(message);
+	}
+	
+	public void sendMailWithReport(String to, String subject, String text, String fileName) {
+		try {
+		MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("noreply@baeldung.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(text,true);
+        FileSystemResource file  = new FileSystemResource(new File(fileName));
+        helper.addAttachment(fileName, file);
+        helper.setSubject(subject);
+		emailSender.send(message);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
